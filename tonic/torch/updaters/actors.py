8e597307d8f596a67f9214c9b5535e8bb904dee3
Buttontheduck <batinmuslucode@gmail.com>
2025-08-19 01:22:31 +0200

    v0.3

--- tonic/torch/updaters/actors.py
+++ tonic/torch/updaters/actors.py
@@ -275,6 +275,203 @@ class TwinCriticSoftDeterministicPolicyGradient:
 
 
 
+class MaximumAPosterioriPolicyOptimization:
+    def __init__(
+        self, num_samples=20, epsilon=1e-1, epsilon_penalty=1e-3,
+        epsilon_mean=1e-3, epsilon_std=1e-6, initial_log_temperature=1.,
+        initial_log_alpha_mean=1., initial_log_alpha_std=10.,
+        min_log_dual=-18., per_dim_constraining=True, action_penalization=True,
+        actor_optimizer=None, dual_optimizer=None, gradient_clip=0
+    ):
+        self.num_samples = num_samples
+        self.epsilon = epsilon
+        self.epsilon_mean = epsilon_mean
+        self.epsilon_std = epsilon_std
+        self.initial_log_temperature = initial_log_temperature
+        self.initial_log_alpha_mean = initial_log_alpha_mean
+        self.initial_log_alpha_std = initial_log_alpha_std
+        self.min_log_dual = torch.as_tensor(min_log_dual, dtype=torch.float32)
+        self.action_penalization = action_penalization
+        self.epsilon_penalty = epsilon_penalty
+        self.per_dim_constraining = per_dim_constraining
+        self.actor_optimizer = actor_optimizer or (
+            lambda params: torch.optim.Adam(params, lr=3e-4))
+        self.dual_optimizer = actor_optimizer or (
+            lambda params: torch.optim.Adam(params, lr=1e-2))
+        self.gradient_clip = gradient_clip
+
+    def initialize(self, model, action_space):
+        self.model = model
+        self.actor_variables = models.trainable_variables(self.model.actor)
+        self.actor_optimizer = self.actor_optimizer(self.actor_variables)
+
+        # Dual variables.
+        self.dual_variables = []
+        self.log_temperature = torch.nn.Parameter(torch.as_tensor(
+            [self.initial_log_temperature], dtype=torch.float32))
+        self.dual_variables.append(self.log_temperature)
+        shape = [action_space.shape[0]] if self.per_dim_constraining else [1]
+        self.log_alpha_mean = torch.nn.Parameter(torch.full(
+            shape, self.initial_log_alpha_mean, dtype=torch.float32))
+        self.dual_variables.append(self.log_alpha_mean)
+        self.log_alpha_std = torch.nn.Parameter(torch.full(
+            shape, self.initial_log_alpha_std, dtype=torch.float32))
+        self.dual_variables.append(self.log_alpha_std)
+        if self.action_penalization:
+            self.log_penalty_temperature = torch.nn.Parameter(torch.as_tensor(
+                [self.initial_log_temperature], dtype=torch.float32))
+            self.dual_variables.append(self.log_penalty_temperature)
+        self.dual_optimizer = self.dual_optimizer(self.dual_variables)
+
+    def __call__(self, observations):
+        def parametric_kl_and_dual_losses(kl, alpha, epsilon):
+            kl_mean = kl.mean(dim=0)
+            kl_loss = (alpha.detach() * kl_mean).sum()
+            alpha_loss = (alpha * (epsilon - kl_mean.detach())).sum()
+            return kl_loss, alpha_loss
+
+        def weights_and_temperature_loss(q_values, epsilon, temperature):
+            tempered_q_values = q_values.detach() / temperature
+            weights = torch.nn.functional.softmax(tempered_q_values, dim=0)
+            weights = weights.detach()
+
+            # Temperature loss (dual of the E-step).
+            q_log_sum_exp = torch.logsumexp(tempered_q_values, dim=0)
+            num_actions = torch.as_tensor(
+                q_values.shape[0], dtype=torch.float32)
+            log_num_actions = torch.log(num_actions)
+            loss = epsilon + (q_log_sum_exp).mean() - log_num_actions
+            loss = temperature * loss
+
+            return weights, loss
+
+        # Use independent normals to satisfy KL constraints per-dimension.
+        def independent_normals(distribution_1, distribution_2=None):
+            distribution_2 = distribution_2 or distribution_1
+            return torch.distributions.independent.Independent(
+                torch.distributions.normal.Normal(
+                    distribution_1.mean, distribution_2.stddev), -1)
+
+        with torch.no_grad():
+            self.log_temperature.data.copy_(
+                torch.maximum(self.min_log_dual, self.log_temperature))
+            self.log_alpha_mean.data.copy_(
+                torch.maximum(self.min_log_dual, self.log_alpha_mean))
+            self.log_alpha_std.data.copy_(
+                torch.maximum(self.min_log_dual, self.log_alpha_std))
+            if self.action_penalization:
+                self.log_penalty_temperature.data.copy_(torch.maximum(
+                    self.min_log_dual, self.log_penalty_temperature))
+
+            target_distributions = self.model.target_actor(observations)
+            actions = target_distributions.sample((self.num_samples,))
+
+            tiled_observations = updaters.tile(observations, self.num_samples)
+            flat_observations = updaters.merge_first_two_dims(
+                tiled_observations)
+            flat_actions = updaters.merge_first_two_dims(actions)
+            values = self.model.target_critic(flat_observations, flat_actions)
+            values = values.view(self.num_samples, -1)
+
+            assert isinstance(
+                target_distributions, torch.distributions.normal.Normal)
+            target_distributions = independent_normals(target_distributions)
+
+        self.actor_optimizer.zero_grad()
+        self.dual_optimizer.zero_grad()
+
+        distributions = self.model.actor(observations)
+        distributions = independent_normals(distributions)
+
+        temperature = torch.nn.functional.softplus(
+            self.log_temperature) + FLOAT_EPSILON
+        alpha_mean = torch.nn.functional.softplus(
+            self.log_alpha_mean) + FLOAT_EPSILON
+        alpha_std = torch.nn.functional.softplus(
+            self.log_alpha_std) + FLOAT_EPSILON
+        weights, temperature_loss = weights_and_temperature_loss(
+            values, self.epsilon, temperature)
+
+        # Action penalization is quadratic beyond [-1, 1].
+        if self.action_penalization:
+            penalty_temperature = torch.nn.functional.softplus(
+                self.log_penalty_temperature) + FLOAT_EPSILON
+            diff_bounds = actions - torch.clamp(actions, -1, 1)
+            action_bound_costs = -torch.norm(diff_bounds, dim=-1)
+            penalty_weights, penalty_temperature_loss = \
+                weights_and_temperature_loss(
+                    action_bound_costs,
+                    self.epsilon_penalty, penalty_temperature)
+            weights += penalty_weights
+            temperature_loss += penalty_temperature_loss
+
+        # Decompose the policy into fixed-mean and fixed-std distributions.
+        fixed_std_distribution = independent_normals(
+            distributions.base_dist, target_distributions.base_dist)
+        fixed_mean_distribution = independent_normals(
+            target_distributions.base_dist, distributions.base_dist)
+
+        # Compute the decomposed policy losses.
+        policy_mean_losses = (fixed_std_distribution.base_dist.log_prob(
+            actions).sum(dim=-1) * weights).sum(dim=0)
+        policy_mean_loss = -(policy_mean_losses).mean()
+        policy_std_losses = (fixed_mean_distribution.base_dist.log_prob(
+            actions).sum(dim=-1) * weights).sum(dim=0)
+        policy_std_loss = -policy_std_losses.mean()
+
+        # Compute the decomposed KL between the target and online policies.
+        if self.per_dim_constraining:
+            kl_mean = torch.distributions.kl.kl_divergence(
+                target_distributions.base_dist,
+                fixed_std_distribution.base_dist)
+            kl_std = torch.distributions.kl.kl_divergence(
+                target_distributions.base_dist,
+                fixed_mean_distribution.base_dist)
+        else:
+            kl_mean = torch.distributions.kl.kl_divergence(
+                target_distributions, fixed_std_distribution)
+            kl_std = torch.distributions.kl.kl_divergence(
+                target_distributions, fixed_mean_distribution)
+
+        # Compute the alpha-weighted KL-penalty and dual losses.
+        kl_mean_loss, alpha_mean_loss = parametric_kl_and_dual_losses(
+            kl_mean, alpha_mean, self.epsilon_mean)
+        kl_std_loss, alpha_std_loss = parametric_kl_and_dual_losses(
+            kl_std, alpha_std, self.epsilon_std)
+
+        # Combine losses.
+        policy_loss = policy_mean_loss + policy_std_loss
+        kl_loss = kl_mean_loss + kl_std_loss
+        dual_loss = alpha_mean_loss + alpha_std_loss + temperature_loss
+        loss = policy_loss + kl_loss + dual_loss
+
+        loss.backward()
+        if self.gradient_clip > 0:
+            torch.nn.utils.clip_grad_norm_(
+                self.actor_variables, self.gradient_clip)
+            torch.nn.utils.clip_grad_norm_(
+                self.dual_variables, self.gradient_clip)
+        self.actor_optimizer.step()
+        self.dual_optimizer.step()
+
+        dual_variables = dict(
+            temperature=temperature.detach(), alpha_mean=alpha_mean.detach(),
+            alpha_std=alpha_std.detach())
+        if self.action_penalization:
+            dual_variables['penalty_temperature'] = \
+                penalty_temperature.detach()
+
+        return dict(
+            policy_mean_loss=policy_mean_loss.detach(),
+            policy_std_loss=policy_std_loss.detach(),
+            kl_mean_loss=kl_mean_loss.detach(),
+            kl_std_loss=kl_std_loss.detach(),
+            alpha_mean_loss=alpha_mean_loss.detach(),
+            alpha_std_loss=alpha_std_loss.detach(),
+            temperature_loss=temperature_loss.detach(),
+            **dual_variables)
+
+
 class DiffusionMaximumAPosterioriPolicyOptimization:
     def __init__(
         self, num_samples=40, epsilon=1e-1, epsilon_penalty=1e-3,
@@ -319,9 +516,10 @@ class DiffusionMaximumAPosterioriPolicyOptimization:
         
 
         # Dual variables.
-        self.dual_variables = []
+
         
         if not self.temperature_dependent:
+            self.dual_variables = []
             self.log_temperature = torch.nn.Parameter(torch.as_tensor(
                 [self.initial_log_temperature], dtype=torch.float32))
             self.dual_variables.append(self.log_temperature)
@@ -510,7 +708,8 @@ class DiffusionMaximumAPosterioriPolicyOptimization:
 
 
         self.actor_optimizer.zero_grad()
-        self.dual_optimizer.zero_grad()
+        if not self.temperature_dependent:
+            self.dual_optimizer.zero_grad()
 
         
         if self.temperature_dependent:
@@ -546,8 +745,11 @@ class DiffusionMaximumAPosterioriPolicyOptimization:
         
 
         policy_loss = score_matching_loss(unbounded_actions.to(self.device),observations,weights,self.denoiser.sigma_data)
-           
+        
+        
         dual_loss = temperature_loss
+
+            
         loss = policy_loss.cpu() + dual_loss
 
         loss.backward()
@@ -560,10 +762,13 @@ class DiffusionMaximumAPosterioriPolicyOptimization:
                 self.dual_variables, self.dual_gradient_clip)
             
         self.actor_optimizer.step()
-        self.dual_optimizer.step()
+        
+        if not self.temperature_dependent:
+            self.dual_optimizer.step()
 
-        dual_variables = dict(
+            dual_variables = dict(
             temperature=temperature.detach())
+            
         if self.action_penalization:
             dual_variables['penalty_temperature'] = \
                 penalty_temperature.detach()
