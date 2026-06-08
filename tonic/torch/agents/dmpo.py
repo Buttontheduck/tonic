Corrected env fed actions from clipping to Tanh. Corrected ScoreMatching Computation

tonic/torch/agents/dmpo.py
@@ -79,12 +79,12 @@ class DMPO(agents.Agent):
     def _step(self, observations):
         observations = torch.as_tensor(observations, dtype=torch.float32)
         with torch.no_grad():
-            return self.model.actor(observations)
+            return torch.tanh(self.model.actor(observations))
 
     def _test_step(self, observations):
         observations = torch.as_tensor(observations, dtype=torch.float32)
         with torch.no_grad():
-            return self.model.actor(observations)
+            return torch.tanh(self.model.actor(observations))
 
     def _update(self, steps):
         keys = ('observations', 'actions', 'next_observations', 'rewards',

tonic/torch/updaters/actors.py
@@ -639,8 +639,9 @@ class DiffusionMaximumAPosterioriPolicyOptimization:
    
             residual = out - (1.0 / c_out) * (action - c_skip*noised_action)  # Equals: ( Denoised_Action - Action )*(1/c_out), Beso uses this
             unweighted_loss = torch.mean(residual**2, dim=-1)
-            loss = unweighted_loss*q_weights
-            return loss.mean()
+            unweighted_loss = unweighted_loss.view(num_sample, batch_size)
+            q_weights = q_weights.view(num_sample, batch_size)
+            return (unweighted_loss*q_weights).sum(dim=0).mean()
 
         def weights_and_temperature_loss(q_values, epsilon, temperature):
             tempered_q_values = q_values.detach() / temperature

tonic/torch/updaters/critics.py
@@ -110,7 +110,7 @@ class DistributionalDeterministicQLearning:
             targets = next_value_distributions.project(returns)
 
         self.optimizer.zero_grad()
-        value_distributions = self.model.critic(observations, torch.tanh(actions))
+        value_distributions = self.model.critic(observations, actions)
         log_probabilities = torch.nn.functional.log_softmax(
             value_distributions.logits, dim=-1)
         loss = -(targets * log_probabilities.to("cpu")).sum(dim=-1).mean()
@@ -320,7 +320,7 @@ class DiffusionExpectedSARSA:
 
 
         self.optimizer.zero_grad()
-        values = self.model.critic(observations, torch.tanh(actions))
+        values = self.model.critic(observations, actions)
         loss = self.loss(returns, values)
 
         loss.backward()